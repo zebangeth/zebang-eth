e08bf5832721883cd22acdff7179507e966fb1e1
zebang.eth <zebang.li.2012@gmail.com>
2024-11-07 14:56:44 -0500

    Add vercel speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { DATA } from "@/data/resume";
 import { cn } from "@/lib/utils";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
@@ -69,6 +70,7 @@ export default function RootLayout({
           </TooltipProvider>
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
